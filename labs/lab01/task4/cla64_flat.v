21552a6ee311fbc3ca144ffaf0724abfa092afbe
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2026-08-29 16:48:25 +0000

    lab01 task4

--- labs/lab01/task4/cla64_flat.v
+++ labs/lab01/task4/cla64_flat.v
@@ -61,11 +61,16 @@ module cla64_flat(
   //
   // TODO: paste your verified assign statements for c[1] through c[64] here.
 
-  assign cout = c[64];
+
+
+  assign #(2) cout = c[64];
 
   // ---------------------------------------------------------------------
   // Step 3: sum bits
   // ---------------------------------------------------------------------
   // TODO: assign #(2) sum = p ^ {c[63:1], cin};
+
+  assign #(2) sum = p ^ {c[63:1], cin};
+
 
 endmodule
