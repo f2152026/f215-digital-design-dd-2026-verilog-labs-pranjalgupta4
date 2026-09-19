c2d59c5d25be5076f6cefc8183b63c4b29ad5cf3
Pranjal Gupta <pranjalgupta58.97@gmail.com>
2026-09-19 12:47:41 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -5,7 +5,23 @@ module tb;
 
   // TODO: declare the inputs and outputs
 
+  parameter WIDTH = 8;
+  parameter DEPTH = 8;
+  reg [$clog2(DEPTH)-1:0] t_sel;
+  wire [WIDTH-1:0] t_dout;
+  reg  t_i0, t_i1, t_s;
+  wire  t_y;
+
+
   // TODO: instantiate DUT here
+
+  lut #(
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH)
+  ) DUT (
+    .sel  (t_sel),
+    .dout (t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -18,7 +34,11 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
-
+    for (integer i = 0; i < DEPTH; i = i + 1) begin
+      t_sel = i;
+      #5;
+    end
+    $finish;
   end
 
   initial
